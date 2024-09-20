55b4bc56fa03bd3a1a158f00ec1c25b15c344a91
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2024-09-19 22:53:20 -0700

    done w env variables

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
-// Your code here 
+
+// your code goes here
+
+require('dotenv').config();
 
 app.get('/', (req, res) => {
     res.send(process.env.SECRET_MESSAGE);
 });
+
+// FOR BONUS -- RUN THIS IN SERVER FOLDER \/
+// SECRET_MESSAGE="Hello from command-line" PORT=5000 node app.js
 
 const port = process.env.PORT;
 app.listen(port, () => console.log('Server is listening on port', port));
